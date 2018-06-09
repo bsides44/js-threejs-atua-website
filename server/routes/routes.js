1a216156de108b2963e5a608d8189d9a928f07c2
maddyking44 <madelineking44@gmail.com>
2018-06-09 15:26:08 +1200

    about page up

---
+++ server/routes/routes.js
@@ -0,0 +1,9 @@
+// const server = require('../server')
+// const express = require('express')
+// const router = express.Router()
+
+// router.get('/ourkaupapa', function (req, res) {
+//         res.render('/ourkaupapa')
+// })
+
+// module.exports = router
--- server/server.js
+++ server/server.js
@@ -3,17 +3,13 @@
 
 const path = require('path')
 const express = require('express')
-// const cors = require('cors')//figure out what this does.
-
-// const weatherRoutes = require('./routes/weather')
-
+// const routes = require('./routes/routes')
 const server = express()
-
-// server.use(cors('*')) //figure out what this does.
 
 server.use(express.static(path.join(__dirname, '../public')))//this connects the server to the front-end, by connecting this server to the index.html file in our public folder.
 
-// server.use('/api/v1/texts', require('./routes/texts'))//we are using /v1 as our api is likely to change.
-// server.use('/api/v1/weather', weatherRoutes)
+// server.use('/', routes)
 
 module.exports = server
+
+
